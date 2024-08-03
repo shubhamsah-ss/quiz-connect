2628c81a6b8d0aeff3279a25f5afbf557d2f443e
Shubham Sah <shubham.6552@gmail.com>
2024-08-03 20:55:39 +0530

    next-feesd

--- components/frontend/Footer.tsx
+++ components/frontend/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Footer = () => {
     return (
         <footer className='w-full bg-white dark:bg-black border-t p-4'>
-            <Link href={"https://next-feed-black.vercel.app/u/quizzconnect"} className='text-blue-500 hover:text-blue-900 hover:underline underline-offset-4 text-xs font-normal float-right whitespace-nowrap'>
+            <Link href={"https://next-feed.devss.in/u/quizzconnect"} className='text-blue-500 hover:text-blue-900 hover:underline underline-offset-4 text-xs font-normal float-right whitespace-nowrap'>
                 Send anonymous message
             </Link>
             {/* <p className='text-xs font-normal text-center whitespace-nowrap dark:text-white'>
